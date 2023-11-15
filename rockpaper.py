7202045ed026129c4b20d1f7849339c2d4a219a6
Jason <140974721+Jason12673@users.noreply.github.com>
2023-11-14 17:00:29 -0800

    Add files via upload

---
+++ rockpaper.py
@@ -0,0 +1,7 @@
+# try this
+decision = input("Rock, Paper, or Scissors? ")
+
+# is there another if condition missing here?
+if decision == "rock" or decision == "paper":
+  # what happens if they give good input? Do we return this value or try again?
+  # write code here
